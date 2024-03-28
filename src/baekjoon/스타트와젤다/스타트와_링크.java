17c446bf9928c351af3a457e4eabebfb92a1a964
DGJSKIM <kjsaj0525@gmail.com>
2024-03-28 10:31:33 +0900

    스타트와 링크 풀이 추가

---
+++ src/baekjoon/스타트와젤다/스타트와_링크.java
@@ -0,0 +1,71 @@
+package baekjoon.스타트와젤다;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class 스타트와_링크 {
+
+    public static int N;
+
+    public static int[][]teamWorks;
+
+    public static boolean[] join;
+    public static int minDiff = Integer.MAX_VALUE;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        teamWorks = new int[N][N];
+        join = new boolean[N];
+        for(int i=0; i<N; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for(int j=0; j<N; j++){
+                teamWorks[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        Arrays.fill(join,false);
+        join[0]= true;
+        dfs(1,1);
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(String.valueOf(minDiff));
+        bw.newLine();
+        bw.flush();
+
+
+
+
+    }
+
+    public static void dfs(int s , int n){
+        if (n == N/2){
+            int startSum = 0;
+            int zeldaSum = 0;
+            for (int i=0; i<N; i++){
+                for (int j=0; j<N; j++){
+                    if(i != j){
+                        if (join[i] && join[j]){
+                            startSum += teamWorks[i][j];
+                        }else if(!join[i]&&!join[j]){
+                            zeldaSum += teamWorks[i][j];
+                        }
+                    }
+
+                }
+            }
+            System.out.println(startSum + " / " + zeldaSum);
+            minDiff = Math.min(minDiff, Math.abs(startSum - zeldaSum));
+            return;
+        }
+        for(int i=s; i<N; i++){
+            if(!join[i]){
+                join[i] = true;
+                dfs(i,n+1);
+                join[i] = false;
+            }
+        }
+
+
+    }
+}
